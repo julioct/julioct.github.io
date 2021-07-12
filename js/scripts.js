77031d7c0cfccad7e89279ca91a452d81794e729
Julio Casal <julioc@dotnetacademy.io>
2021-07-12 09:19:09 -0700

    Do not let scroll load crash when navbar is not present

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,52 @@
 */
 
 
-(function($) {
-    "use strict"; 
-	
+(function ($)
+{
+    "use strict";
+
     /* Navbar Scripts */
     // jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 60) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
+    $(window).on('scroll load', function ()
+    {
+        if ($('.navbar').length)
+        {
+            if ($(".navbar").offset().top > 60)
+            {
+                $(".fixed-top").addClass("top-nav-collapse");
+            } else
+            {
+                $(".fixed-top").removeClass("top-nav-collapse");
+            }
+        }
     });
-    
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function ()
+    {
+        $(document).on('click', 'a.page-scroll', function (event)
+        {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 600, 'easeInOutExpo');
+            event.preventDefault();
+        });
     });
 
     // offcanvas script from Bootstrap + added element to close menu on click in small viewport
-    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function ()
+    {
         $('.offcanvas-collapse').toggleClass('open')
     })
 
     // hover in desktop mode
-    function toggleDropdown (e) {
+    function toggleDropdown(e)
+    {
         const _d = $(e.target).closest('.dropdown'),
             _m = $('.dropdown-menu', _d);
-        setTimeout(function(){
+        setTimeout(function ()
+        {
             const shouldOpen = e.type !== 'click' && _d.is(':hover');
             _m.toggleClass('show', shouldOpen);
             _d.toggleClass('show', shouldOpen);
@@ -45,8 +57,8 @@
         }, e.type === 'mouseleave' ? 300 : 0);
     }
     $('body')
-    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
-    .on('click', '.dropdown-menu a', toggleDropdown);
+        .on('mouseenter mouseleave', '.dropdown', toggleDropdown)
+        .on('click', '.dropdown-menu a', toggleDropdown);
 
 
     /* Image Slider - Swiper */
@@ -54,11 +66,11 @@
         autoplay: {
             delay: 2000,
             disableOnInteraction: false
-		},
+        },
         loop: true,
         spaceBetween: 30,
         slidesPerView: 5,
-		breakpoints: {
+        breakpoints: {
             // when window is <= 575px
             575: {
                 slidesPerView: 1,
@@ -85,32 +97,32 @@
 
 
     /* Text Slider - Swiper */
-	var textSlider = new Swiper('.text-slider', {
+    var textSlider = new Swiper('.text-slider', {
         autoplay: {
             delay: 6000,
             disableOnInteraction: false
-		},
+        },
         loop: true,
         navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev'
-		}
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
     });
 
-    
+
     /* Details Lightbox - Magnific Popup */
     $('.popup-with-move-anim').magnificPopup({
-		type: 'inline',
-		fixedContentPos: true,
-		fixedBgPos: true,
-		overflowY: 'auto',
-		closeBtnInside: true,
-		preloader: false,
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
+        type: 'inline',
+        fixedContentPos: true,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
     });
-    
+
 
     /* Video Lightbox - Magnific Popup */
     $('.popup-youtube, .popup-vimeo').magnificPopup({
@@ -123,19 +135,21 @@
         iframe: {
             patterns: {
                 youtube: {
-                    index: 'youtube.com/', 
-                    id: function(url) {        
+                    index: 'youtube.com/',
+                    id: function (url)
+                    {
                         var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
-                        if ( !m || !m[1] ) return null;
+                        if (!m || !m[1]) return null;
                         return m[1];
                     },
                     src: 'https://www.youtube.com/embed/%id%?autoplay=1'
                 },
                 vimeo: {
-                    index: 'vimeo.com/', 
-                    id: function(url) {        
+                    index: 'vimeo.com/',
+                    id: function (url)
+                    {
                         var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
-                        if ( !m || !m[5] ) return null;
+                        if (!m || !m[5]) return null;
                         return m[5];
                     },
                     src: 'https://player.vimeo.com/video/%id%?autoplay=1'
@@ -147,62 +161,75 @@
 
     /* Move Form Fields Label When User Types */
     // for input and textarea fields
-    $("input, textarea").keyup(function(){
-		if ($(this).val() != '') {
-			$(this).addClass('notEmpty');
-		} else {
-			$(this).removeClass('notEmpty');
-		}
-	});
-	
+    $("input, textarea").keyup(function ()
+    {
+        if ($(this).val() != '')
+        {
+            $(this).addClass('notEmpty');
+        } else
+        {
+            $(this).removeClass('notEmpty');
+        }
+    });
+
 
     /* Back To Top Button */
     // create the back to top button
     $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
     var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
+    $(window).scroll(function ()
+    {
+        if ($(window).scrollTop() > amountScrolled)
+        {
             $('a.back-to-top').fadeIn('500');
-        } else {
+        } else
+        {
             $('a.back-to-top').fadeOut('500');
         }
     });
 
 
-	/* Removes Long Focus On Buttons */
-	$(".button, a, button").mouseup(function() {
-		$(this).blur();
-	});
+    /* Removes Long Focus On Buttons */
+    $(".button, a, button").mouseup(function ()
+    {
+        $(this).blur();
+    });
 
     /* Counter - CountTo */
-	var a = 0;
-	$(window).scroll(function() {
-		if ($('#counter').length) { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
-			var oTop = $('#counter').offset().top - window.innerHeight;
-			if (a == 0 && $(window).scrollTop() > oTop) {
-			$('.counter-value').each(function() {
-				var $this = $(this),
-				countTo = $this.attr('data-count');
-				$({
-				countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				},
-				{
-					duration: 2000,
-					easing: 'swing',
-					step: function() {
-					    $this.text(Math.floor(this.countNum));
-					},
-					complete: function() {
-					    $this.text(this.countNum);
-					    //alert('finished');
-					}
-				});
-			});
-			a = 1;
-			}
-		}
-    });    
+    var a = 0;
+    $(window).scroll(function ()
+    {
+        if ($('#counter').length)
+        { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
+            var oTop = $('#counter').offset().top - window.innerHeight;
+            if (a == 0 && $(window).scrollTop() > oTop)
+            {
+                $('.counter-value').each(function ()
+                {
+                    var $this = $(this),
+                        countTo = $this.attr('data-count');
+                    $({
+                        countNum: $this.text()
+                    }).animate({
+                        countNum: countTo
+                    },
+                        {
+                            duration: 2000,
+                            easing: 'swing',
+                            step: function ()
+                            {
+                                $this.text(Math.floor(this.countNum));
+                            },
+                            complete: function ()
+                            {
+                                $this.text(this.countNum);
+                                //alert('finished');
+                            }
+                        });
+                });
+                a = 1;
+            }
+        }
+    });
 
 })(jQuery);
