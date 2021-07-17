b8e290014597c4bc26250b9ddd0970350a75e55d
Julio Casal <julioc@dotnetacademy.io>
2021-07-17 08:07:08 -0700

    Disable back to top button

--- js/scripts.js
+++ js/scripts.js
@@ -175,18 +175,18 @@
 
     /* Back To Top Button */
     // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function ()
-    {
-        if ($(window).scrollTop() > amountScrolled)
-        {
-            $('a.back-to-top').fadeIn('500');
-        } else
-        {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
+    // $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    // var amountScrolled = 700;
+    // $(window).scroll(function ()
+    // {
+    //     if ($(window).scrollTop() > amountScrolled)
+    //     {
+    //         $('a.back-to-top').fadeIn('500');
+    //     } else
+    //     {
+    //         $('a.back-to-top').fadeOut('500');
+    //     }
+    // });
 
 
     /* Removes Long Focus On Buttons */
